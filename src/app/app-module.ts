72157fc34b28d4cbb2dbb6176b7202168deeafd9
Vinicius Santos <vinicius.bessa9420@gmail.com>
2025-08-26 09:02:49 -0300

    Initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Product } from './product/product';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    App,
+    Product
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/product/product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: false,
+  templateUrl: './product.html',
+  styleUrl: './product.css'
+})
+export class Product {
+
+}
